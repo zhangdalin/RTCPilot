842531dd32e164c1cdf2b117fb89c48428775ede
zhangdalin <zdl5406@163.com>
2026-01-26 02:01:47 -0800

    update logger to add LINE FUNCTION FILENAME

--- src/utils/logger.hpp
+++ src/utils/logger.hpp
@@ -3,6 +3,7 @@
 #include "timeex.hpp"
 
 #include <string>
+#include <string.h>
 #include <stdint.h>
 #include <stdarg.h>
 #include <cstdio> // std::snprintf()
@@ -12,6 +13,13 @@
 #include <sstream>
 #include <iostream>
 #include <vector>
+#include <thread>
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#include <sys/syscall.h>
+#endif
 
 namespace cpp_streamer
 {
@@ -25,6 +33,15 @@ enum LOGGER_LEVEL {
     LOGGER_ERROR_LEVEL
 };
 
+#define LogError(logger, data) LogErrorfEx(logger, __FUNCTION__, __FILE__, __LINE__, data)
+#define LogWarn(logger, data)  LogWarnfEx(logger, __FUNCTION__, __FILE__, __LINE__, data)
+#define LogInfo(logger, data)  LogInfofEx(logger, __FUNCTION__, __FILE__, __LINE__, data)
+#define LogDebug(logger, data) LogDebugfEx(logger, __FUNCTION__, __FILE__, __LINE__, data)
+#define LogErrorf(logger, fmt, ...) LogErrorfEx(logger, __FUNCTION__, __FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#define LogWarnf(logger, fmt, ...)  LogWarnfEx(logger, __FUNCTION__, __FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#define LogInfof(logger, fmt, ...)  LogInfofEx(logger, __FUNCTION__, __FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#define LogDebugf(logger, fmt, ...) LogDebugfEx(logger, __FUNCTION__, __FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#define LogInfoData(logger, data, len, dscr) LogInfoDataEx(logger, __FUNCTION__, __FILE__, __LINE__, data, len, dscr)
 
 class Logger
 {
@@ -64,13 +81,31 @@ public:
     size_t BufferSize() {
         return buffer_len_;
     }
-    void Logf(const char* level, const char* buffer) {
+
+    void Logf(const char* level, const char* func, const char* file, int line, const char* buffer) {
         std::stringstream ss;
-
-
+        unsigned long long tid = 0;
+#ifdef _WIN32
+        tid = static_cast<unsigned long long>(::GetCurrentThreadId());
+#elif defined(__linux__)
+        tid = static_cast<unsigned long long>(::syscall(SYS_gettid));
+#else
+        tid = std::hash<std::thread::id>{}(std::this_thread::get_id());
+#endif
+        const char* filename = file;
+        const char* slash1 = strrchr(file, '/');
+        const char* slash2 = strrchr(file, '\\');
+        if (slash1 && slash2)
+            filename = (slash1 > slash2) ? slash1 + 1 : slash2 + 1;
+        else if (slash1)
+            filename = slash1 + 1;
+        else if (slash2)
+            filename = slash2 + 1;
         ss << "[" << level << "]" << "[" << get_now_str() << "]"
-           << buffer << "\r\n";
-        
+           << "[TID:" << tid << "]"
+           << "[" << func << "]"
+           << buffer
+           << "[" << filename << ":" << line << "] " << "\r\n";
         if (filename_.empty()) {
             std::cout << ss.str();
         } else {
@@ -98,100 +133,61 @@ private:
     size_t buffer_len_ = LOGGER_BUFFER_SIZE;
 };
 
-inline void LogError(Logger* logger, const char* data) {
-    if (logger == nullptr || logger->GetLevel() > LOGGER_INFO_LEVEL) {
-        return;
-    }
-    logger->Logf("W", data);
-}
-
-inline void LogErrorf(Logger* logger, const char* fmt, ...) {
+inline void LogErrorfEx(Logger* logger, const char* func, const char* file, int line, const char* fmt, ...) {
     if (logger == nullptr || logger->GetLevel() > LOGGER_ERROR_LEVEL) {
         return;
     }
     char* buffer = logger->GetBuffer();
     size_t bsize = logger->BufferSize();
     va_list ap;
- 
     va_start(ap, fmt);
     int ret_len = vsnprintf(buffer, bsize, fmt, ap);
     buffer[ret_len] = 0;
     va_end(ap);
-
-    logger->Logf("E", buffer);
+    logger->Logf("E", func, file, line, buffer);
 }
 
-inline void LogWarn(Logger* logger, const char* data) {
-    if (logger == nullptr || logger->GetLevel() > LOGGER_INFO_LEVEL) {
-        return;
-    }
-    logger->Logf("W", data);
-}
-
-inline void LogWarnf(Logger* logger, const char* fmt, ...) {
+inline void LogWarnfEx(Logger* logger, const char* func, const char* file, int line, const char* fmt, ...) {
     if (logger == nullptr || logger->GetLevel() > LOGGER_WARN_LEVEL) {
         return;
     }
     char* buffer = logger->GetBuffer();
     size_t bsize = logger->BufferSize();
     va_list ap;
- 
     va_start(ap, fmt);
     vsnprintf(buffer, bsize, fmt, ap);
     va_end(ap);
-
-    logger->Logf("W", buffer);
+    logger->Logf("W", func, file, line, buffer);
 }
 
-inline void LogInfo(Logger* logger, const char* data) {
-    if (logger == nullptr || logger->GetLevel() > LOGGER_INFO_LEVEL) {
-        return;
-    }
-    logger->Logf("I", data);
-}
-
-inline void LogInfof(Logger* logger, const char* fmt, ...) {
+inline void LogInfofEx(Logger* logger, const char* func, const char* file, int line, const char* fmt, ...) {
     if (logger == nullptr || logger->GetLevel() > LOGGER_INFO_LEVEL) {
         return;
     }
     char* buffer = logger->GetBuffer();
     size_t bsize = logger->BufferSize();
     va_list ap;
- 
     va_start(ap, fmt);
     vsnprintf(buffer, bsize, fmt, ap);
-    //int ret_len = vsnprintf(buffer, bsize, fmt, ap);
-    //buffer[ret_len] = 0;
     va_end(ap);
-
-    //std::cout << "loginfo size:" << bsize << ", str len:" << ret_len << "\r\n\r\n";
-    logger->Logf("I", buffer);
+    logger->Logf("I", func, file, line, buffer);
 }
 
-inline void LogDebug(Logger* logger, const char* data) {
-    if (logger == nullptr || logger->GetLevel() > LOGGER_INFO_LEVEL) {
-        return;
-    }
-    logger->Logf("D", data);
-}
-
-inline void LogDebugf(Logger* logger, const char* fmt, ...) {
+inline void LogDebugfEx(Logger* logger, const char* func, const char* file, int line, const char* fmt, ...) {
     if (logger == nullptr || logger->GetLevel() > LOGGER_DEBUG_LEVEL) {
         return;
     }
     char* buffer = logger->GetBuffer();
     size_t bsize = logger->BufferSize();
     va_list ap;
- 
     va_start(ap, fmt);
     int ret_len = vsnprintf(buffer, bsize, fmt, ap);
     buffer[ret_len] = 0;
     va_end(ap);
-
-    logger->Logf("D", buffer);
+    logger->Logf("D", func, file, line, buffer);
 }
 
-inline void LogInfoData(Logger* logger, const uint8_t* data, size_t len, const char* dscr) {
+inline void LogInfoDataEx(Logger* logger, const char* func, const char* file, int line, const uint8_t* data, size_t len, const char* dscr) {
     if (!logger || logger->GetLevel() > LOGGER_INFO_LEVEL) {
         return;
     }
@@ -203,13 +199,13 @@ inline void LogInfoData(Logger* logger, const uint8_t* data, size_t len, const c
     }
     size_t print_len = 0;
     const int MAX_LINES = 500;
-    int line = 0;
+    int nline = 0;
     int index = 0;
     print_len += snprintf(print_data, print_buffer_size, "%s:", dscr);
     for (index = 0; index < (int)len; index++) {
         if ((index%16) == 0) {
             print_len += snprintf(print_data + print_len, print_buffer_size - print_len, "\r\n");
-            if (++line > MAX_LINES) {
+            if (++nline > MAX_LINES) {
                 break;
             }
         }
@@ -217,8 +213,7 @@ inline void LogInfoData(Logger* logger, const uint8_t* data, size_t len, const c
             " %02x", *(static_cast<const uint8_t*>(data + index)));
     }
 
-
-    logger->Logf("I", print_data);
+    logger->Logf("I", func, file, line, print_data);
 
     delete[] print_data;
 }
